9669eaba8547c47e16bcb1bcee6ad61fc61e2a84
Rajesh10 <guddewadi10@gmail.com>
2024-05-21 22:33:21 +0530

    useEffect Hooks
    useEffect Hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 
 function App() {
   
-  let [Counter,setCounter]=useState(10)
-  const addvalue = ()=>{
-    if (Counter < 20){
-       setCounter(Counter+1);
-    }
-  }
-  const removeValue = () => {
-    if (Counter > 0) {
-      setCounter(Counter - 1); 
-    }
-  };
+  let [Counter,setCounter]=useState(0)
+  
+  useEffect(() => {
+    setTimeout(() => {
+      if (Counter < 20) {
+        setCounter((Counter) => Counter + 1);
+      }
+    },1000);
+  }, [Counter]);
+  // const addvalue = ()=>{
+  //   if (Counter < 20){
+  //      setCounter(Counter+1);
+  //   }
+  // }
+  // const removeValue = () => {
+  //   if (Counter > 0) {
+  //     setCounter(Counter - 1); 
+  //   }
+  // };
   return (
     <div className="App">
       <h1>Counter using hooks</h1>
 
       <h2>Counter : {Counter}</h2>
       <br />
-      <div >
+       {/* 
+       <div >
+      <h1>hello world</h1>
       <button style={{ margin: '10px' }} onClick={addvalue}>Add Value</button>
       <button style={{ margin: '10px' }} onClick={removeValue}>Remove Value</button>
-      </div>
+  </div> 
+*/}
       
     </div>
     
   );
-
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Stateuse from './components/UseState/stateuse'; // Assuming 'stateuse' is a custom component
+//import Stateuse from './components/UseState/stateuse'; // Assuming 'stateuse' is a custom component
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-    <Stateuse />
-  </React.StrictMode>
+    // <Stateuse />
+  // </React.StrictMode>
 );
